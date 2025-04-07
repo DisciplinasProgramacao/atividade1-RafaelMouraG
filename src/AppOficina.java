e8841797f41ea0b6487087ee72bd669b894cd94c
RafaelMouraG <rafaelganascinidemoura@gmail.com>
2025-04-07 11:28:59 -0300

    Requisitos 1 e 2. Adição do arquivo dos produtos

--- src/AppOficina.java
+++ src/AppOficina.java
@@ -88,7 +88,8 @@ public class AppOficina {
     static int exibirMenuOrdenadores() {
         cabecalho();
         System.out.println("1 - Bolha");
-        System.out.println("2 - Inserção");     
+        System.out.println("2 - Inserção");
+        System.out.println("3 - Mergesort");     
         System.out.println("0 - Finalizar");
        
         return lerNumero("Digite sua opção", Integer.class);
@@ -172,6 +173,7 @@ public class AppOficina {
         switch (opcao) {
             case 1 -> ordenador = new Bubblesort<>();
             case 2 -> ordenador = new InsertSort<>();
+            case 3-> ordenador = new Mergesort<>();
         }
 
         switch (ordencao) {
--- src/ComparadorPorValor.java
+++ src/ComparadorPorValor.java
@@ -13,6 +13,11 @@ public class ComparadorPorValor implements Comparator<Produto>{
         else if (o1.valorDeVenda()<o2.valorDeVenda()){
             resposta =-1;
         }
+       else if (o1.compareTo(o2)==1) {
+        resposta = 1;
+       }
+            
+        
         return resposta;
     }
 }
